9de5507fc57f9e0c8227c8158d58be62e4e3b9e5
Kai Hwang <hwangk@arnold.wpic.upmc.edu>
2014-04-16 12:23:04 -0400

    another path prob.

--- Clock_Maxfilter_Script.sh
+++ Clock_Maxfilter_Script.sh
@@ -9,7 +9,7 @@ for s in 10637 10662 10997 11243 11246 11252 11253 11255 11258 11262 11263; do
 		-f /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}_clock_run${r}_raw.fif \
 		-o /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}_clock_run${r}_raw_chpi_sss.fif \
 		-origin fit -autobad off \
-		-bad $(sed s/MEG// /home/hwangk/Luna1/Multimodal/Clock/${s}/${s}_clock_bad_run${r}.txt) \
+		-bad $(sed s/MEG// /home/hwangk/Luna1/Multimodal/Clock/MEG/${s}/${s}_clock_bad_run${r}.txt) \
 		-st 10 -movecomp inter -v \
 		-ctc /home/hwangk/Luna1/ni_tools/maxfilter_calib/ct_sparse.fif \
 		-cal /home/hwangk/Luna1/ni_tools/maxfilter_calib/sss_cal.dat \
@@ -17,7 +17,7 @@ for s in 10637 10662 10997 11243 11246 11252 11253 11255 11258 11262 11263; do
 		
 		/neuro/bin/util/i686-pc-linux-gnu/maxfilter-2.2 \
 		-f /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}_clock_run${r}_raw_chpi_sss.fif \
-		-o /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}_clock_run${r}_ds_sss_raw.fif \
+				-o /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}_clock_run${r}_ds_sss_raw.fif \
 		-origin fit -trans default -frame head -force -v -autobad off -ds 4 \
 		> /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}_clock_run${r}_trans.log
 	
