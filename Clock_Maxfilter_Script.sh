d933d238458b2768fe3da440c9d8d9e2076548ee
Kai Hwang <hwangk@arnold.wpic.upmc.edu>
2014-04-16 12:18:27 -0400

    fix path prob.

--- Clock_Maxfilter_Script.sh
+++ Clock_Maxfilter_Script.sh
@@ -9,17 +9,17 @@ for s in 10637 10662 10997 11243 11246 11252 11253 11255 11258 11262 11263; do
 		-f /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}_clock_run{r}_raw.fif \
 		-o /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}_clock_run{r}_raw_chpi_sss.fif \
 		-origin fit -autobad off \
-		-bad $(sed s/MEG// ${s}_clock_bad_run${r}.txt) \
+		-bad $(sed s/MEG// /home/hwangk/Luna1/Multimodal/Clock/${s}/${s}_clock_bad_run${r}.txt) \
 		-st 10 -movecomp inter -v \
 		-ctc /home/hwangk/Luna1/ni_tools/maxfilter_calib/ct_sparse.fif \
 		-cal /home/hwangk/Luna1/ni_tools/maxfilter_calib/sss_cal.dat \
-		> /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}/MEG/${s}_clock_run${r}_sss.log
+		> /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}_clock_run${r}_sss.log
 		
 		/neuro/bin/util/i686-pc-linux-gnu/maxfilter-2.2 \
 		-f /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}_clock_run{r}_raw_chpi_sss.fif \
 		-o /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}_clock_run{r}_ds_sss_raw.fif \
 		-origin fit -trans default -frame head -force -v -autobad off -ds 4 \
-		> /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}/MEG/${s}_clock_run${r}_trans.log
+		> /home/hwangk/Luna1/Multimodal/Clock/${s}/MEG/${s}_clock_run${r}_trans.log
 	
 	done
 done
