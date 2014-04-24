5f6529915546d626a7027a60ec903fb2cb62d887
Kai Hwang <hwangk@wallace.wpic.upmc.edu>
2014-04-24 13:51:02 -0400

    create head model.

---
+++ Clock_head_model_wrapper.sh
@@ -0,0 +1,13 @@
+
+export SUBJECTS_DIR=/home/hwangk/Luna1/Multimodal/Clock
+for s in 11258 11255 ; do
+
+	while [`jobs | wc -l` -ge 8 ]
+  	do
+    		sleep 10
+  	done
+	
+	cd $SUBJECTS_DIR
+	make_mne_model.sh ${s} > $SUBJECTS_DIR/${s}/scripts/${s}_make_mne_model.log 2>&1 &	
+
+done
