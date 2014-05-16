69531abbd6bcbd0999307573b16d8c9e3d25c577
Kai Hwang <kai.hwang@gmail.com>
2014-05-16 08:03:48 -0400

    fit subject data then save R output.

--- model.R
+++ model.R
@@ -10,48 +10,51 @@ if (!require(fitclock)) {
 library(ggplot2)
 library(plyr)
 
-#subjdata <- read.csv('11243_20140213_1.csv',header=T)
-subjdata <- adply(Sys.glob('subjs/10637_20140312/*csv'),1,.fun=function(x){read.csv(x,header=T)})
-#setup subject
-subj10637 <- clockdata_subject(subject_ID="10637", dataset=subjdata)
+#list of subjects to fit
+Subj <- c(10997, 11243, 11246, 11255, 11258, 11262, 11263)
 
-#setup model to fit RT
-RT_model <- clock_model()
+#loop through subjects and fit
+for (s in Subj) 
+	{ 
+	subjdata <- adply(Sys.glob(paste("/Volumes/T800/Multimodal/Clock",s,"MEG/*csv",sep="/")),1,.fun=function(x){read.csv(x,header=T)})
+	#setup subject
+	subData <- clockdata_subject(subject_ID=as.character(s), dataset=subjdata)
+	
 
-RT_model$add_params(
-       meanRT(max_value=4000),
-       autocorrPrevRT(),
-       goForGold(),
-       go(),
-       noGo(),
-       meanSlowFast(),
-       exploreBeta()
-    )
+	#setup model to fit RT
+	RT_model <- clock_model()
 
-#setup model to fit RT differences
-expDiff_model <- clock_model(fit_RT_diffs=TRUE)
-expDiff_model$add_params(
-       meanRT(max_value=4000),
-       autocorrPrevRT(),
-       goForGold(),
-       go(),
-       noGo(),
-       meanSlowFast(),
-       exploreBeta()
-    )
+	RT_model$add_params(
+       	meanRT(max_value=4000),
+       	autocorrPrevRT(),
+       	goForGold(),
+       	go(),
+       	noGo(),
+       	meanSlowFast(),
+       	exploreBeta()
+    	)
 
-#tell model which dataset to use
-RT_model$set_data(subj10637)
-expDiff_model$set_data(subj10637)
+	#setup model to fit RT differences
+	expDiff_model <- clock_model(fit_RT_diffs=TRUE)
+	expDiff_model$add_params(
+       	meanRT(max_value=4000),
+       	autocorrPrevRT(),
+       	goForGold(),
+       	go(),
+       	noGo(),
+       	meanSlowFast(),
+       	exploreBeta()
+    	)
 
-# #test the incremental contribution of each parameter to AIC (fit)
-# incr_fit <- expDiff_model$incremental_fit(njobs=6)
-# 
-# #vector of AIC values
-# AICs<-sapply(incr_fit$incremental_fits, "[[", "AIC")
+	#tell model which dataset to use
+	RT_model$set_data(subData)
+	expDiff_model$set_data(subData)
 
-#fit full model, using 5 random starts and choosing the best fit
-fitRT <- RT_model$fit(random_starts=5)
-fitDiffRT <- expDiff_model$fit(random_starts=5)
+	#fit full model, using 5 random starts and choosing the best fit
+	fitRT <- RT_model$fit(random_starts=5)
+	fitDiffRT <- expDiff_model$fit(random_starts=5)
+	
+	#save data
+	save(file=paste("subjs/",s,"_fitdata.Rdata",sep=""),fitRT,fitDiffRT,s)
 
-
+}
