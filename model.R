aa01725021c5ccaa3e117cd4eaf31136e03605a7
Kai Hwang <kai.hwang@gmail.com>
2014-05-20 12:08:31 -0400

    check output before running the fit.

--- model.R
+++ model.R
@@ -15,46 +15,61 @@ Subj <- c(10997, 11243, 11246, 11255, 11258, 11262, 11263)
 
 #loop through subjects and fit
 for (s in Subj) 
-	{ 
-	subjdata <- adply(Sys.glob(paste("/Volumes/T800/Multimodal/Clock",s,"MEG/*csv",sep="/")),1,.fun=function(x){read.csv(x,header=T)})
-	#setup subject
-	subData <- clockdata_subject(subject_ID=as.character(s), dataset=subjdata)
+	{
+  #setup file name
+  file=paste("subjs/",s,"_fitdata.Rdata",sep="")
+  
+  #only run fitting if output not saved already
+  if (!file.exists(file))
+    {
+  
+	  subjdata <- adply(Sys.glob(paste("/Volumes/T800/Multimodal/Clock",s,"MEG/*csv",sep="/")),1,.fun=function(x){read.csv(x,header=T)})
+	  #setup subject
+	  subData <- clockdata_subject(subject_ID=as.character(s), dataset=subjdata)
 	
 
-	#setup model to fit RT
-	RT_model <- clock_model()
+	  #setup model to fit RT
+	  RT_model <- clock_model()
 
-	RT_model$add_params(
-       	meanRT(max_value=4000),
-       	autocorrPrevRT(),
-       	goForGold(),
-       	go(),
-       	noGo(),
-       	meanSlowFast(),
-       	exploreBeta()
-    	)
+	  RT_model$add_params(
+         	meanRT(max_value=4000),
+         	autocorrPrevRT(),
+       	  goForGold(),
+       	  go(),
+       	  noGo(),
+       	  meanSlowFast(),
+       	  exploreBeta()
+    	  )
 
-	#setup model to fit RT differences
-	expDiff_model <- clock_model(fit_RT_diffs=TRUE)
-	expDiff_model$add_params(
-       	meanRT(max_value=4000),
-       	autocorrPrevRT(),
-       	goForGold(),
-       	go(),
-       	noGo(),
-       	meanSlowFast(),
-       	exploreBeta()
-    	)
+	  #setup model to fit RT differences
+	  expDiff_model <- clock_model(fit_RT_diffs=TRUE)
+	  expDiff_model$add_params(
+       	  meanRT(max_value=4000),
+       	  autocorrPrevRT(),
+       	  goForGold(),
+       	  go(),
+       	  noGo(),
+       	  meanSlowFast(),
+       	  exploreBeta()
+    	  )
 
-	#tell model which dataset to use
-	RT_model$set_data(subData)
-	expDiff_model$set_data(subData)
+	  #tell model which dataset to use
+	  RT_model$set_data(subData)
+	  expDiff_model$set_data(subData)
 
-	#fit full model, using 5 random starts and choosing the best fit
-	fitRT <- RT_model$fit(random_starts=5)
-	fitDiffRT <- expDiff_model$fit(random_starts=5)
+	  #fit full model, using 5 random starts and choosing the best fit
+	  fitRT <- RT_model$fit(random_starts=5)
+	  fitDiffRT <- expDiff_model$fit(random_starts=5)
 	
-	#save data
-	save(file=paste("subjs/",s,"_fitdata.Rdata",sep=""),fitRT,fitDiffRT,s)
-
+	  #save data
+	  save(file,fitRT,fitDiffRT,s)
+    }
+  
+  #code for plotting
+  load(file)
+  type=''
+  type[1:63]<-'rpe'
+  type[64:126]<-'reward'
+  D<-data.frame("Trial Number"=c(1:63,1:63),data=c(data=fitRT$rpe[4,],reward=fitRT$Reward[4,]),type)
+  qplot(Trial.Number,data,data=D,color=type,geom="line", xlab="Trial Number", ylab="Magnitude", main=S)
 }
