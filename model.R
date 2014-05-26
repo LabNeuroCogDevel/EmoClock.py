0f9724fb3112f30115a33bc79a10e099c30ceb33
Kai Hwang <kai.hwang@gmail.com>
2014-05-26 10:21:38 -0400

    convert rt swing and epsilon to z score, report correlation between RT swing and exploration.

--- model.R
+++ model.R
@@ -87,12 +87,22 @@ for (s in Subj)
     rt_type=rep(c("observed", "predicted"), each=length(fitRT$RTobs))
   )
   
+  #transform exploration parameter and RT swing
+  epsilon_beta <- as.vector(t(laply(fitDiffRT$pred_contrib[1:nrow(fitDiffRT$RTobs)],"[","p_epsilonBeta",1:ncol(fitRT$RTobs))))  
+  epsilon_beta[is.na(epsilon_beta)] <- 0 #take out nans
+  epsilon_beta <- (epsilon_beta-mean(epsilon_beta))/sd(epsilon_beta);
+  rt_swing <- as.vector(t(fitDiffRT$RTobs));
+  rt_swing[is.na(rt_swing)] <- 0
+  rt_swing <- (rt_swing-mean(rt_swing))/sd(rt_swing);
+  corr <- cor(epsilon_beta,rt_swing,use='complete.obs')
+  
   DiffRTFitDf <- data.frame(
     run=rep(1:nrow(fitDiffRT$RTobs), each=ncol(fitDiffRT$RTobs)),
     trialN=1:ncol(fitRT$RTobs),
     emo_condition=rep(fitDiffRT$run_condition, each=ncol(fitDiffRT$RTobs)),
     rew_function=rep(fitDiffRT$rew_function, each=ncol(fitDiffRT$RTobs)),
     RT=c(as.vector(t(fitDiffRT$RTobs)),as.vector(t(laply(fitDiffRT$pred_contrib[1:nrow(fitDiffRT$RTobs)],"[","p_epsilonBeta",1:ncol(fitRT$RTobs))))),
+    RT_z=c(as.vector(rt_swing),as.vector(epsilon_beta)),
     rt_type=rep(c("RT Diff", "Exploration"), each=length(fitRT$RTobs))
   )  
   
@@ -113,13 +123,14 @@ for (s in Subj)
   print(RTobsVRTpred_Fig)
   
   #plot exploration
-  RTDiffVExp_Fig <- ggplot(DiffRTFitDf, aes(trialN,RT,colour=rt_type))
+  RTDiffVExp_Fig <- ggplot(DiffRTFitDf, aes(trialN,RT_z,colour=rt_type))
   RTDiffVExp_Fig <- RTDiffVExp_Fig+layer(geom="line") + facet_grid(rew_function ~ emo_condition)
-  title = paste(s,' RT Swing vs. Exploration Parameter')
+  title = paste(s,' RT Swing vs. Exploration Parameter','  ','Correlation = ', sprintf("%.4f",corr))
   RTDiffVExp_Fig <- RTDiffVExp_Fig + ggtitle (title) + theme (legend.title = element_blank()) + 
-    xlab("Trial Number") +ylab("RT (ms)")
+    xlab("Trial Number") +ylab("z score")
   print(RTDiffVExp_Fig)
   
-  #print(qplot(,data,data=D,color=type,geom="line", xlab="Trial Number", ylab="Magnitude", main=s)) 
 }
 dev.off()
+
+
